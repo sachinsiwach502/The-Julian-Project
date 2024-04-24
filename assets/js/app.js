30690ca4616cd42edaece8dc8a17dfc9e9919a68
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-04-24 11:39:43 +0530

    complelet why history section

--- assets/js/app.js
+++ assets/js/app.js
@@ -74,3 +74,146 @@ tabBtn3.addEventListener("click", () => {
 });
 
 
+// why history----------------------------
+let changingTxt = document.querySelector(".changing_paragraph");
+let changingHeading = document.querySelector(".changingHeading");
+let clickPart1 = document.querySelector(".clickPart1");
+let clickPart2 = document.querySelector(".clickPart2");
+let clickPart3 = document.querySelector(".clickPart3");
+let clickPart4 = document.querySelector(".clickPart4");
+let clickPart5 = document.querySelector(".clickPart5");
+let clickPart6 = document.querySelector(".clickPart6");
+let clickPart7 = document.querySelector(".clickPart7");
+let clickPart8 = document.querySelector(".clickPart8");
+let changing_img_color = document.querySelector(".changing_arrow");
+let clickTab = document.querySelectorAll(".histry_card_border");
+let sideLine = document.querySelector(".sideline");
+let txtcolor = document.querySelector(".txt-color-change ");
+
+sideLine.style.opacity = "1";
+txtcolor.classList.add("timeline-heading");
+
+changing_img_color.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M19 12L13 18M19 12L13 6M19 12L5 12" stroke="url(#paint0_linear_50775_599)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<defs>
+<linearGradient id="paint0_linear_50775_599" x1="19" y1="6" x2="4.69489" y2="16.925" gradientUnits="userSpaceOnUse">
+<stop stop-color="#F4EAD8"/>
+<stop offset="1" stop-color="#E0B764"/>
+</linearGradient>
+</defs>
+</svg>
+`
+
+clickTab.forEach(tabs => {
+    let changing_img_color = tabs.querySelector(".changing_arrow");
+    let txtcolor = tabs.querySelector(".txt-color-change ");
+    let sideLine = tabs.querySelector(".sideline");
+    tabs.addEventListener("click", () => {
+        clickTab.forEach(otherTabs => {
+            let changing_img_color = otherTabs.querySelector(".changing_arrow");
+            let txtcolor = otherTabs.querySelector(".txt-color-change ");
+            let sideLine = otherTabs.querySelector(".sideline");
+            if (otherTabs !== tabs) {
+                txtcolor.classList.remove("timeline-heading");
+                sideLine.style.opacity = "0";
+                changing_img_color.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M20.3635 12.7083C20.5509 12.5208 20.6562 12.2665 20.6562 12.0013C20.6562 11.7362 20.5509 11.4818 20.3635 11.2943L14.7065 5.63732C14.6142 5.54181 14.5039 5.46563 14.3819 5.41322C14.2599 5.36081 14.1286 5.33322 13.9959 5.33207C13.8631 5.33091 13.7314 5.35622 13.6085 5.4065C13.4856 5.45678 13.374 5.53103 13.2801 5.62492C13.1862 5.71882 13.1119 5.83047 13.0616 5.95337C13.0114 6.07626 12.9861 6.20794 12.9872 6.34072C12.9884 6.4735 13.016 6.60472 13.0684 6.72672C13.1208 6.84873 13.197 6.95907 13.2925 7.05132L17.2425 11.0013L3.99946 11.0013C3.73425 11.0013 3.47989 11.1067 3.29236 11.2942C3.10482 11.4817 2.99946 11.7361 2.99946 12.0013C2.99946 12.2665 3.10482 12.5209 3.29236 12.7084C3.47989 12.896 3.73425 13.0013 3.99946 13.0013L17.2425 13.0013L13.2925 16.9513C13.1103 17.1399 13.0095 17.3925 13.0118 17.6547C13.0141 17.9169 13.1192 18.1677 13.3046 18.3531C13.4901 18.5385 13.7409 18.6437 14.0031 18.646C14.2653 18.6483 14.5179 18.5475 14.7065 18.3653L20.3635 12.7083Z" fill="#FCF9F6" fill-opacity="0.7"/>
+</svg>
+`
+            }
+        });
+        txtcolor.classList.add("timeline-heading")
+        sideLine.style.opacity = "1"
+
+        changing_img_color.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M19 12L13 18M19 12L13 6M19 12L5 12" stroke="url(#paint0_linear_50775_599)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<defs>
+<linearGradient id="paint0_linear_50775_599" x1="19" y1="6" x2="4.69489" y2="16.925" gradientUnits="userSpaceOnUse">
+<stop stop-color="#F4EAD8"/>
+<stop offset="1" stop-color="#E0B764"/>
+</linearGradient>
+</defs>
+</svg>
+`
+    })
+})
+
+clickPart1.addEventListener("click", () => {
+    changingHeading.innerHTML = `Political History`;
+    changingHeading.classList.add("timeline-heading");
+    changingTxt.innerHTML = `The origins of political history systems can be traced back to 
+    ancient civilizations, where early forms of governance and societal organization emerged.
+     One of the earliest political systems was found in ancient Mesopotamia, where city-states
+      developed complex administrative structures and written legal codes. The ancient Egyptians
+       also established centralized authority with the pharaohs ruling as both political and
+        religious leaders. In ancient Greece,
+    the city-state of Athens laid the groundwork for democratic principles, with
+     citizens participating in decision-making processes. The Roman Republic, with its system
+      of checks and balances, influenced later Western political thought.`
+
+});
+clickPart2.addEventListener("click", () => {
+    changingHeading.innerHTML = `Military History`;
+    changingHeading.classList.add("timeline-heading");
+    changingTxt.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci sequi tempora maxime et inventore ea, vel
+    voluptatem eveniet, molestias amet tempore libero dolor nobis qui veritatis enim ipsam, saepe labore?`
+});
+clickPart3.addEventListener("click", () => {
+    changingHeading.innerHTML = `Gender History`;
+    changingHeading.classList.add("timeline-heading");
+    changingTxt.innerHTML = `The origins of political history systems can be traced back to 
+    ancient civilizations, where early forms of governance and societal organization emerged.
+     One of the earliest political systems was found in ancient Mesopotamia, where city-states
+      developed complex administrative structures and written legal codes. The ancient Egyptians
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci sequi tempora maxime et inventore ea, vel
+    voluptatem eveniet, molestias amet tempore libero dolor nobis qui veritatis enim ipsam, saepe labore?`
+});
+clickPart4.addEventListener("click", () => {
+    changingHeading.innerHTML = `Ecoomic History`;
+    changingHeading.classList.add("timeline-heading");
+    changingTxt.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci sequi tempora maxime et inventore ea, vel
+    voluptatem eveniet, molestias amet tempore libero dolor nobis qui veritatis enim ipsam, saepe labore?
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci sequi tempora maxime et inventore ea, vel
+    voluptatem eveniet, molestias amet tempore libero dolor nobis qui veritatis enim ipsam, saepe labore?`
+});
+clickPart5.addEventListener("click", () => {
+    changingHeading.innerHTML = `Environmntal History`;
+    changingHeading.classList.add("timeline-heading");
+    changingTxt.innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quis pariatur recusandae qui aperiam laborum in.
+    Repudiandae asperiores temporibus alias molestias deserunt labore expedita neque rerum esse harum repellendus ipsam
+    maiores iste distinctio est veritatis adipisci nemo, delectus in. Reprehenderit, praesentium reiciendis omnis veniam
+    magnam, possimus temporibus mollitia impedit, alias similique fugit dolorem unde beatae. Quidem nostrum repellat
+    unde facilis corrupti aliquid, atque autem enim recusandae ipsum ex doloremque excepturi?`
+});
+
+clickPart6.addEventListener("click", () => {
+    changingHeading.innerHTML = `Cultural  History`;
+    changingHeading.classList.add("timeline-heading");
+    changingTxt.innerHTML = `The origins of political history systems can be traced back to 
+    ancient civilizations,Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quis pariatur recusandae qui aperiam laborum in.
+    Repudiandae asperiores temporibus alias molestias deserunt labore expedita neque rerum esse harum repellendus ipsam
+    maiores iste distinctio est veritatis adipisci nemo, delectus in. Reprehenderit, praesentium reiciendis omnis veniam
+    magnam, possimus temporibus mollitia impedit, alias similique fugit dolorem unde beatae. Quidem nostrum repellat
+    unde facilis corrupti aliquid, atque autem enim recusandae ipsum ex doloremque excepturi?`
+});
+clickPart7.addEventListener("click", () => {
+    changingHeading.innerHTML = `Social History`;
+    changingHeading.classList.add("timeline-heading");
+    changingTxt.innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quis pariatur recusandae qui aperiam laborum in.
+    Repudiandae asperiores temporibus alias molestias deserunt labore expedita neque rerum esse harum repellendus ipsam
+    maiores iste distinctio est veritatis adipisci nemo, delectus in. Reprehenderit, praesentium reiciendis omnis veniam
+    magnam, possimus temporibus mollitia impedit, alias similique fugit dolorem unde beatae.`
+});
+clickPart8.addEventListener("click", () => {
+    changingHeading.innerHTML = `Intellectual History`;
+    changingHeading.classList.add("timeline-heading");
+    changingTxt.innerHTML = `The origins of political history systems can be traced back to 
+    ancient civilizations, where early forms of governance and societal organization emerged.
+     One of the earliest political systems was found in ancient Mesopotamia, where city-states
+      developed complex administrative structures and written legal codes. The ancient Egyptians
+       alsoLorem ipsum dolor sit amet consectetur adipisicing elit. Rem quis pariatur recusandae qui aperiam laborum in.
+    Repudiandae asperiores temporibus alias molestias deserunt labore expedita neque rerum esse harum repellendus ipsam
+    maiores iste distinctio est veritatis adipisci nemo, delectus in. Reprehenderit, praesentium reiciendis omnis veniam
+    magnam, possimus temporibus mollitia impedit, alias similique fugit dolorem unde beatae. Quidem nostrum repellat
+    unde facilis corrupti aliquid, atque autem enim recusandae ipsum ex doloremque excepturi?`
+});
