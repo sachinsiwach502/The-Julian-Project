5f7a6716694668f035623f02c3ea011c2c15f272
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-04-25 11:38:21 +0530

    complete the slick slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -254,6 +254,45 @@ accrodian.forEach(items => {
     });
 });
 
+// slick slider-------------------------------
+$('.slick_slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: ".next",
+    prevArrow:".prev",
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
 
 
 
